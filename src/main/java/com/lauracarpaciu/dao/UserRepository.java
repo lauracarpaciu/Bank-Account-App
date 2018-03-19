a70178fdbab70ee581725841e2de2f89af9e2bab
Laura Carpaciu <mirelacarpaciu@yahoo.com>
2018-03-19 12:57:56 +0200

    create userRepository

--- src/main/java/com/lauracarpaciu/dao/UserRepository.java
+++ src/main/java/com/lauracarpaciu/dao/UserRepository.java
@@ -1,12 +1,9 @@
 package com.lauracarpaciu.dao;
 
-import com.lauracarpaciu.entities.account.Account;
 import com.lauracarpaciu.entities.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-
-public interface UserRepository extends JpaRepository<User,String>{
-
+public interface UserRepository extends JpaRepository<User,Long>{
+    User findUserByUsername(@Param("username") String username);
 }
