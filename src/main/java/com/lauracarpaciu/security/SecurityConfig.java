14cc6a1db3efa09f9073057490644154cfb2d5f3
Laura Carpaciu <mirelacarpaciu@yahoo.com>
2018-03-24 16:02:25 +0200

    security

--- src/main/java/com/lauracarpaciu/security/SecurityConfig.java
+++ src/main/java/com/lauracarpaciu/security/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
