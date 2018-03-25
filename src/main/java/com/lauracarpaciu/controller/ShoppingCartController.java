5ef10e977ee096face69ea518866cc66fdaec52d
Laura Carpaciu <mirelacarpaciu@yahoo.com>
2018-03-25 09:19:23 +0300

    controller.shoppingCart

---
+++ src/main/java/com/lauracarpaciu/controller/ShoppingCartController.java
@@ -0,0 +1,34 @@
+package com.lauracarpaciu.controller;
+
+import com.lauracarpaciu.entities.cart.CartEvent;
+import com.lauracarpaciu.entities.user.User;
+import com.lauracarpaciu.service.ShoppingCartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class ShoppingCartController {
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+    @RequestMapping(path = "/shoppingCart", method = RequestMethod.POST)
+    public ResponseEntity addCartEvent(@RequestBody CartEvent cartEvent,@RequestBody User user) throws Exception {
+        return Optional.ofNullable(shoppingCartService.addCartEvent(cartEvent,user))
+                .map(event -> new ResponseEntity(HttpStatus.NO_CONTENT))
+                .orElseThrow(() -> new Exception("Could not find shopping cart"));
+    }
+
+    @RequestMapping(path = "/shoppingCart", method = RequestMethod.POST)
+    public ResponseEntity addCartEvent(@RequestBody CartEvent cartEvent,@RequestBody String userName) throws Exception {
+        return Optional.ofNullable(shoppingCartService.addCartEvent(cartEvent,userName))
+                .map(event -> new ResponseEntity(HttpStatus.NO_CONTENT))
+                .orElseThrow(() -> new Exception("Could not find shopping cart"));
+    }
+}
