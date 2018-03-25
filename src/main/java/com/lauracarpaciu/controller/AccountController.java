0a0dd348232fa8e73147cb3d997f50bf24507019
Laura Carpaciu <mirelacarpaciu@yahoo.com>
2018-03-25 08:52:44 +0300

    controller.account

--- src/main/java/com/lauracarpaciu/controller/AccountController.java
+++ src/main/java/com/lauracarpaciu/controller/AccountController.java
@@ -19,8 +19,8 @@ public class AccountController {
     private AccountService accountService;
 
     @RequestMapping(value = "/accounts", method = RequestMethod.GET)
-    public ResponseEntity getUserAccount() throws Exception {
-        return Optional.ofNullable(accountService.getUserAccounts())
+    public ResponseEntity getUserAccount(String userName) throws Exception {
+        return Optional.ofNullable(accountService.getUserAccounts(userName))
                 .map(a -> new ResponseEntity<List<Account>>(a, HttpStatus.OK))
                 .orElseThrow(() -> new Exception("Accounts for user do not exist"));
     }
