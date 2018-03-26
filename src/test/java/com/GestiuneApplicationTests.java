6180a8ed18a5c76cf6edb3126bd9e752f9c9d5a1
Laura Carpaciu <mirelacarpaciu@yahoo.com>
2018-03-26 09:01:07 +0300

    test

--- src/test/java/com/GestiuneApplicationTests.java
+++ src/test/java/com/GestiuneApplicationTests.java
@@ -61,33 +61,22 @@ public class GestiuneApplicationTests {
     public void customerTest() {
         log.info("*** Starting Customer Test");
 
-        // Create a new account
         Account account = new Account("5", "12345");
-
-        // Create a new customer for the account
         Customer customer = new Customer("Rx224", "Laura", "Carpaciu", "laura.carpaciu@gmail.com", "07255333", account);
 
-        // Create a new credit card for the account
         CreditCard creditCard = new CreditCard("1234567801234567", new CurrentAccount("CC1", 800, 20), CreditCardType.VISA);
 
-        // Add the credit card to the customer's account
         customer.getAccount()
                 .getCreditCards()
                 .add(creditCard);
 
-        // Create a new shipping address for the customer
         Address address = new Address("Aleea Poaiana Sibiului", null,
                 "Bucuresti", "Romania", 20500, AddressType.SHIPPING);
 
-        // Add address to the customer's account
         customer.getAccount()
                 .getAddresses()
                 .add(address);
-
-        // Apply the cascading update by persisting the customer object
         customer = customerRepository.save(customer);
-
-        // Query for the customer object to ensure cascading persistence of the object graph
         log.info(customerRepository.findOne(customer.getId()).toString());
     }
 
@@ -232,8 +221,6 @@ public class GestiuneApplicationTests {
             Assert.assertTrue(e.getMessage(), false);
         }
     }
-
-
 
 
 }
