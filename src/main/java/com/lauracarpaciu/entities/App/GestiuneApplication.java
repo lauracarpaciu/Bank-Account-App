a90d53822aec8204dbc54f3075406f0979f49b07
Laura Carpaciu <mirelacarpaciu@yahoo.com>
2018-03-29 09:31:59 +0300

    readme

--- src/main/java/com/lauracarpaciu/entities/App/GestiuneApplication.java
+++ src/main/java/com/lauracarpaciu/entities/App/GestiuneApplication.java
@@ -1,13 +1,16 @@
 package com.lauracarpaciu.entities.App;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
+@EnableAutoConfiguration
+@ComponentScan(basePackages="com.lauracarpaciu.entities.App")
+@EnableJpaRepositories("com.lauracarpaciu.dao")
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableJpaRepositories
 public class GestiuneApplication {
 
 	public static void main(String[] args) {
